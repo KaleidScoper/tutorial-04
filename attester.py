2da68e6f6b9fcee9d8d14ac98e84322e582db5a6
Kaleid Scoper <68382243+KaleidScoper@users.noreply.github.com>
2024-12-10 01:42:08 +0800

    修改元组

--- attester.py
+++ attester.py
@@ -3,11 +3,7 @@
 import utils
 
 def attest(aik_keypair):
-    # 示证者生成报告
+    private_key_aik, public_key_aik = aik_keypair  # 解包元组
     report_data = utils.generate_report_data()
-    
-    # 使用AIK私钥签名报告
-    report_signature = utils.sign(report_data, aik_keypair['private_key'])
-    
-    # 返回报告数据和签名
-    return {'report': report_data, 'report_signature': report_signature, 'aik_public_key': aik_keypair['public_key']}, report_signature
+    report_signature = utils.sign(report_data, private_key_aik)
+    return report_data, report_signature
--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import verifier
 import attester
 
 def main():
-    # 初始化
+    # 初始化 CA
     ca_obj = ca.CA()
 
     # 示证者注册
     public_key_attester, signature_attester = ca_obj.register_attester()
 
     # 平台身份证书颁发
-    aik_keypair, signature_aik = ca_obj.issue_aik(public_key_attester)
+    aik_keypair, signature_ca_aik = ca_obj.issue_aik(public_key_attester)
 
-    # 示例远程证明
-    report, report_signature = attester.attest(aik_keypair)
-    verifier.verify_report(report, report_signature, public_key_attester, ca_obj.public_key)
+    # 示证者生成报告并签名
+    report, report_signature = attester.attest(aik_keypair[0])  # 传递密钥对
+
+    # 验证者验证报告
+    verifier.verify_report(report, report_signature, aik_keypair[0][1], signature_ca_aik, ca_obj.public_key)
 
 if __name__ == "__main__":
     main()
