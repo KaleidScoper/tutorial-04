66588bfa6cb19edc9891423fd7ea0c86636f190d
Kaleid Scoper <68382243+KaleidScoper@users.noreply.github.com>
2024-12-10 01:45:53 +0800

    还有bug就直接睡觉

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     aik_keypair, signature_ca_aik = ca_obj.issue_aik(public_key_attester)
 
     # 示证者生成报告并签名
-    report, report_signature = attester.attest(aik_keypair[0])  # 传递密钥对
+    report, report_signature = attester.attest(aik_keypair[0])  # 传递 (private_key_aik, public_key_aik)
 
     # 验证者验证报告
     verifier.verify_report(report, report_signature, aik_keypair[0][1], signature_ca_aik, ca_obj.public_key)
