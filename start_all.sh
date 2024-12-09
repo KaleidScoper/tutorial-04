fe654f4202c771b17e33fd9bb9fb185bda60a8c5
Kaleid Scoper <68382243+KaleidScoper@users.noreply.github.com>
2024-12-10 01:05:30 +0800

    更新路径
    忘了改路径

--- start_all.sh
+++ start_all.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # 启动CA
-python3 /path/to/tutorial-04/ca.py &
+python3 ./ca.py &
 
 # 等待CA启动
 sleep 1
 
 # 启动示证者
-python3 /path/to/tutorial-04/client.py &
+python3 ./client.py &
 
 # 等待示证者启动
 sleep 1
 
 # 启动证明者
-python3 /path/to/tutorial-04/prover.py
+python3 ./prover.py
