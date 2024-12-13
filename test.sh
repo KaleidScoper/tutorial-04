d7b2aa49eee140e016015272238f518474929d74
Kaleid Scoper <68382243+KaleidScoper@users.noreply.github.com>
2024-12-13 17:21:41 +0800

    Create test.sh
    由于测试服务器访问不了GitHub，先新增了一个脚本半自动化解压缩。以后再研究代理。

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 此脚本并非启动or测试脚本，与项目功能无关。
+
+rm -rf tutorial-04
+rm -rf tutorial-04-main
+sleep 1
+unzip tutorial-04-main.zip
+sleep 1
+mv tutorial-04-main tutorial-04
+sleep 1
+rm *.zip
+sleep 1
+ls -l
+exit 0
